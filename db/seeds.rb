80c207f9ff5a533571435b2ff18e6be90484abe2
Christina Finsterwalder <finsti@gmx.de>
2018-09-18 11:27:45 +0200

    4.2 added colour and price fields, added de locale

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Charly LI-ION POWERHEAT", description: "Elektrisch beheizbare, weiche Lederhandschuhe zum Gleitschirmfliegen und für Outdoor-Aktivitäten im Winter.", image_url: "https://images-na.ssl-images-amazon.com/images/I/71hs2IY8T7L._SL1000_.jpg")
+
+Product.create(name: "Charly LI-ION LIGHT", description: "Elektrisch beheizbare, dünne, speziell für den Gleitschirm-, Langlauf- und Reitsport entwickelte Lederhandschuhe.", image_url: "https://images-na.ssl-images-amazon.com/images/I/61yZiPJ%2BLmL._SL1000_.jpg")
+
+Product.create(name: "Charly LI-ION FIRE PLUS", description: "Elektrisch beheizbare, dünne Handschuhe, die sowohl als Unterziehhandschuhe in Fäustlingen als auch alleine getragen werden können.", image_url: "https://images-na.ssl-images-amazon.com/images/I/61ry05607uL._SL1000_.jpg")
