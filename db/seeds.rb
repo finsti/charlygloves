91b970c0ed932bba16dbe14a2abc03db308e303d
Christina Finsterwalder <finsti@gmx.de>
2018-09-18 11:48:02 +0200

    4.2 added colour and price fields, added de locale

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(name: "Charly LI-ION POWERHEAT", description: "Elektrisch beheizbare, weiche Lederhandschuhe zum Gleitschirmfliegen und für Outdoor-Aktivitäten im Winter.", image_url: "https://images-na.ssl-images-amazon.com/images/I/71hs2IY8T7L._SL1000_.jpg")
+Product.create(name: "Charly LI-ION POWERHEAT", description: "Elektrisch beheizbare, weiche Lederhandschuhe zum Gleitschirmfliegen und für Outdoor-Aktivitäten im Winter.", colour: "black", price: 239.5, image_url: "https://images-na.ssl-images-amazon.com/images/I/71hs2IY8T7L._SL1000_.jpg")
 
-Product.create(name: "Charly LI-ION LIGHT", description: "Elektrisch beheizbare, dünne, speziell für den Gleitschirm-, Langlauf- und Reitsport entwickelte Lederhandschuhe.", image_url: "https://images-na.ssl-images-amazon.com/images/I/61yZiPJ%2BLmL._SL1000_.jpg")
+Product.create(name: "Charly LI-ION LIGHT", description: "Elektrisch beheizbare, dünne, speziell für den Gleitschirm-, Langlauf- und Reitsport entwickelte Lederhandschuhe.", colour: "black", price: 209.5, image_url: "https://images-na.ssl-images-amazon.com/images/I/61yZiPJ%2BLmL._SL1000_.jpg")
 
-Product.create(name: "Charly LI-ION FIRE PLUS", description: "Elektrisch beheizbare, dünne Handschuhe, die sowohl als Unterziehhandschuhe in Fäustlingen als auch alleine getragen werden können.", image_url: "https://images-na.ssl-images-amazon.com/images/I/61ry05607uL._SL1000_.jpg")
+Product.create(name: "Charly LI-ION FIRE PLUS", description: "Elektrisch beheizbare, dünne Handschuhe, die sowohl als Unterziehhandschuhe in Fäustlingen als auch alleine getragen werden können.", colour: "black", price: 179.8, image_url: "https://images-na.ssl-images-amazon.com/images/I/61ry05607uL._SL1000_.jpg")
