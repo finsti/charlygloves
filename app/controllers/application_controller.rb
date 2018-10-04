7eca938a99e8f4fb40d05be192ac2834545a7187
Christina Finsterwalder <finsti@gmx.de>
2018-10-04 22:13:39 +0200

    5.1 updated site root, implemented redirect from lp address to site root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :redirect_lp_to_root
+
+  def redirect_lp_to_root
+    if request.path.end_with?('/landing_page')
+      redirect_to :root
+    end
+  end
 end
