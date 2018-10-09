df366891daf777c30be55c94393be3c78a39c08d
Christina Finsterwalder <finsti@gmx.de>
2018-10-09 11:33:36 +0200

    5.2 implemented search in navbar, updated products CRUD pages

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   def self.search(search_term)
-    Product.where("name LIKE ?", "%#{search_term}%")
+    Product.where("lower(name) LIKE lower(?)", "%#{search_term}%")
   end
 end
