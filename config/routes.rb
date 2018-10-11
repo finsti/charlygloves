04ab66937e3e83c8ccf7c1faf246add8ac2022b7
Christina Finsterwalder <finsti@gmx.de>
2018-10-11 10:55:54 +0200

    5.3 added contact form

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/landing_page'
+  post 'simple_pages/thank_you'
   # get 'simple_pages/index'
   root 'simple_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
