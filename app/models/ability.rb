2b5d1e44616631c4dd30aacd268e92864ab615da
Christina Finsterwalder <finsti@gmx.de>
2018-10-22 11:49:02 +0200

    5.10 added admin moderation functionality

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,14 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
-    can :manage, Order, id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, User, id: user.id
+      can :manage, Order, id: user.id
+      can :manage, Comment, user_id: user.id
+      cannot [:destroy, :delete], Comment
+    end
   end
-  
+
 end
