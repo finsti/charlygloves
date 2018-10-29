5b24ac3cbc89e4d8ed89d74bf6ea317db9753b0d
Christina Finsterwalder <finsti@gmx.de>
2018-10-29 12:21:27 +0100

    6.6 implemented payments controller

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,8 @@ class UserMailer < ApplicationMailer
   default from: 'vlinfo@gmx.net'
 
   def new_registration(user)
-    mail( to: user.email,
-          subject: 'Welcome to Charly Gloves!')
+    mail(to: user.email,
+         subject: 'Welcome to Charly Gloves!')
   end
 
   def thank_you
@@ -14,6 +14,13 @@ class UserMailer < ApplicationMailer
       to: 'vlinfo@gmx.net',
       subject: "A new contact form message from #{@name}",
       body: @message).deliver_now
+  end
+
+  def order_placed(user, product)
+    @user = user
+    @product = product
+    mail(to: user.email,
+         subject: "Thank you for your order!")
   end
 
 end
