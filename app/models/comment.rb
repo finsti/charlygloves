4d26f87ee1ff34a08b09ec5d56669d8c52b8364c
Christina Finsterwalder <finsti@gmx.de>
2018-11-06 22:23:22 +0100

    6.14 improved app with Active Job and Action Cable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,12 +3,14 @@ class Comment < ApplicationRecord
   belongs_to :product
   belongs_to :user
 
+  scope :rating_asc, -> { order(rating: :asc) }
+  scope :rating_desc, -> { order(rating: :desc) }
+
   validates :body, presence: true
   validates :user, presence: true
   validates :product, presence: true
   validates :rating, numericality: { only_integer: true }
 
-  scope :rating_asc, -> { order(rating: :asc) }
-  scope :rating_desc, -> { order(rating: :desc) }
+  after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
 
 end
